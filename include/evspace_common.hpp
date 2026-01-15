52ed5ef80b5ebb0a75c964570c13a84fc4f0f8a1
Quinton Barnes <quinton.barnes68@gmail.com>
2026-01-15 01:11:33 -0600

    included gsl/span for pre c++20 span support

---
+++ include/evspace_common.hpp
@@ -0,0 +1,27 @@
+// This file includes gsl::span from Microsoft GSL
+// License: MIT, https://github.com/microsoft/GSL
+
+#ifndef _EVSPACE_COMMON_H_
+#define _EVSPACE_COMMON_H_
+
+#if __cplusplus >= 202002L
+#include <span>
+#else
+#include <gsl/span.h>
+#endif
+
+namespace evspace {
+    
+#if __cplusplus >= 202002L
+template<typename T>
+using span_t = std::span<T>;
+#define EVSPACE_USING_STD_SPAN
+#else
+template<typename T>
+using span_t = gsl::span<T>;
+#define EVSPACE_USING_GSL_SPAN
+#endif // _EVSPACE_COMMON_H_
+
+}
+
+#endif // _EVSPACE_COMMON_H_
